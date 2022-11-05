0e979b57f9107ffecca558c486281dc08965db4b
jona-tech <jonathanbernhardsson1@gmail.com>
2022-11-05 15:15:34 +0000

    add counter and fix bugs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// code structure from https://www.youtube.com/watch?v=riDzcEQbX6k with intention to adjust
+// code structure from https://www.youtube.com/watch?v=riDzcEQbX6k with a few adjustments
 
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
@@ -6,13 +6,20 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+
 let shuffledQuestions, currentQuestionIndex
+let scoreBox = document.getElementById('score')
 
 startButton.addEventListener('click', startQuiz)
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
-})
+});
+
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('count-correct').innerText)
+    document.getElementById('count-correct').innerText = ++oldScore
+};
 
 function startQuiz() {
     startButton.classList.add('hide')
@@ -20,15 +27,15 @@ function startQuiz() {
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
-}
+};
 
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+};
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
@@ -38,26 +45,34 @@ function showQuestion(question) {
         }
         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
-    });
-}
+    })
+};
 
 function resetState() {
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
-}
+};
 
 function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
+    let selectedButton = e.target;
+    let correct = selectedButton.dataset.correct;
+    if (correct) {
+        incrementScore()
+    }
     setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    nextButton.classList.remove('hide')
-}
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        questionContainerElement.classList.add('hide')
+    }
+};
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
@@ -66,78 +81,211 @@ function setStatusClass(element, correct) {
     } else {
         element.classList.add('wrong')
     }
-}
+};
 
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
-}
+};
 
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function checkAnswer() {
-
-}
-
-const questions = [
-    {
+const questions = [{
         question: 'How many tones exist in the chromatic scale?',
-        answers: [
-            { text: '8', correct: false},
-            { text: '9', correct: false},
-            { text: '14', correct: false},
-            { text: '12', correct: true}
+        answers: [{
+                text: '8',
+                correct: false
+            },
+            {
+                text: '9',
+                correct: false
+            },
+            {
+                text: '14',
+                correct: false
+            },
+            {
+                text: '12',
+                correct: true
+            }
         ]
     },
     {
         question: 'Who wrote the song "Blinded By The Light"?',
-        answers: [
-            { text: 'Bob Dylan', correct: false},
-            { text: 'Bob Marley', correct: false},
-            { text: 'Bruce Springsteen', correct: true},
-            { text: 'Manfred Mann', correct: false}
+        answers: [{
+                text: 'Bob Dylan',
+                correct: false
+            },
+            {
+                text: 'Bob Marley',
+                correct: false
+            },
+            {
+                text: 'Bruce Springsteen',
+                correct: true
+            },
+            {
+                text: 'Manfred Mann',
+                correct: false
+            }
         ]
     },
     {
         question: 'Where does the "Nyckelharpa" come from?',
-        answers: [
-            { text: 'Sweden', correct: true},
-            { text: 'Norway', correct: false},
-            { text: 'Denmark', correct: false},
-            { text: 'Finland', correct: false}
+        answers: [{
+                text: 'Sweden',
+                correct: true
+            },
+            {
+                text: 'Norway',
+                correct: false
+            },
+            {
+                text: 'Denmark',
+                correct: false
+            },
+            {
+                text: 'Finland',
+                correct: false
+            }
         ]
     },
     {
         question: 'Where does the "Hardingfela" come from?',
-        answers: [
-            { text: 'Sweden', correct: false},
-            { text: 'Norway', correct: true},
-            { text: 'Denmark', correct: false},
-            { text: 'Finland', correct: false}
+        answers: [{
+                text: 'Sweden',
+                correct: false
+            },
+            {
+                text: 'Norway',
+                correct: true
+            },
+            {
+                text: 'Denmark',
+                correct: false
+            },
+            {
+                text: 'Finland',
+                correct: false
+            }
         ]
     },
     {
         question: 'What is the average radio output frequency',
-        answers: [
-            { text: '432 Hz', correct: false},
-            { text: '528 Hz', correct: false},
-            { text: '420 Hz', correct: false},
-            { text: '440 Hz', correct: true}
+        answers: [{
+                text: '432 Hz',
+                correct: false
+            },
+            {
+                text: '528 Hz',
+                correct: false
+            },
+            {
+                text: '420 Hz',
+                correct: false
+            },
+            {
+                text: '440 Hz',
+                correct: true
+            }
         ]
     },
     {
         question: 'What is a "Stradivarius"?',
-        answers: [
-            { text: 'Violin', correct: true},
-            { text: 'Piano', correct: false},
-            { text: 'Program', correct: false},
-            { text: 'Guitar', correct: false}
+        answers: [{
+                text: 'Violin',
+                correct: true
+            },
+            {
+                text: 'Piano',
+                correct: false
+            },
+            {
+                text: 'Program',
+                correct: false
+            },
+            {
+                text: 'Guitar',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Who shot the sheriff?',
+        answers: [{
+                text: 'I shot the sheriff',
+                correct: true
+            },
+            {
+                text: 'You shot the sheriff',
+                correct: false
+            },
+            {
+                text: 'He shot the sheriff',
+                correct: false
+            },
+            {
+                text: 'He shot himself',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is the standard tuning of a guitar?',
+        answers: [{
+                text: 'DADGAD',
+                correct: false
+            },
+            {
+                text: 'CDEFGA',
+                correct: false
+            },
+            {
+                text: 'EADGBE',
+                correct: true
+            },
+            {
+                text: 'GEADBE',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Which of these is a DAW',
+        answers: [{
+                text: 'Nuendo',
+                correct: true
+            },
+            {
+                text: 'Cubase',
+                correct: true
+            },
+            {
+                text: 'Ableton',
+                correct: true
+            },
+            {
+                text: 'Logic',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'What instrument did Niccolo Paganini play?',
+        answers: [{
+                text: 'Drums',
+                correct: false
+            },
+            {
+                text: 'Piano',
+                correct: false
+            },
+            {
+                text: 'Violin',
+                correct: true
+            },
+            {
+                text: 'Guitar',
+                correct: false
+            }
         ]
     }
 ]
